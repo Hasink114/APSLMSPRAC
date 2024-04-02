b5bdac35887a74ab87b7fd92578c18732a6c7023
Hasink114 <111485995+Hasink114@users.noreply.github.com>
2024-04-02 19:58:40 +0500

    .

--- js/dashboard.js
+++
@@ -1,6 +0,0 @@
-const jsonDataString = localStorage.getItem("jsonData");
-const jsonData = JSON.parse(jsonDataString);
-console.log(jsonData.response.Name);
-const stdname = jsonData.response.Name;
-document.getElementById("StudentName").innerText = stdname;
-document.getElementById("marks").innerText = jsonData.response.Maths;
---
+++ script/script.js
@@ -0,0 +1,12 @@
+/*document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.ctaa');
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            alert('Files will be uploaded after 1:00 pm.');
+        });
+    });
+});
+*/
+
+alert("New Practice paper will be uploaded before 3 days of next paper.");
