11bb56636b855211a04064cbb65a0e1b1a167f7a
Hasink114 <111485995+Hasink114@users.noreply.github.com>
2024-04-21 09:06:54 +0500

    .

--- script/script.js
+++ script/script.js
@@ -1,12 +1,5 @@
-/*document.addEventListener('DOMContentLoaded', function() {
-    const links = document.querySelectorAll('.ctaa');
-    links.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault();
-            alert('Files will be uploaded after 1:00 pm.');
-        });
-    });
-});
-*/
+const jsonDataStringID = localStorage.getItem("jsonUserID");
+const jsonUserID = JSON.parse(jsonDataStringID);
 
+console.log(jsonUserID);
 alert("New Practice paper is uploaded.");
